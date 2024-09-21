5289ac19a157eb6867fb0887963b92e6b86d6478
dfficult <157505714+dfficult@users.noreply.github.com>
2024-09-21 20:32:53 +0800

    Add files via upload
    Fixed bot.change_presence not working
    Updated /eat, adding amount so you don't have to type the command 30 times

---
+++ eat.py
@@ -0,0 +1,47 @@
+# === eat.py ===
+import discord
+from discord import app_commands
+from discord.app_commands import Range
+import random
+from typing import Optional
+
+# --- Prefixs ---
+prefixs = ["就吃","我覺得","必須是","吃","為什麼不來點","今晚，我想來點","我推薦","幫我點","好久沒吃","跟你說我要吃"]
+ends = ["吧","是個不錯的選擇","的吧","就對了","呢?","\n(應該) 都點的到","","，謝謝 <3","了","，到底要問幾次"]
+
+
+# --- Command: eat ---
+@app_commands.command(name="eat", description=f"[隨機] 從食物清單中挑選出一種食物")
+@app_commands.describe(amount="輸入數量")
+async def eat(interaction: discord.Interaction, amount: Optional[Range[int, 1, 30]]):
+    with open("foodlist.txt","r",encoding="UTF-8") as f:
+        contents = f.readlines()
+        foodlist = [i.strip() for i in contents]
+    if not amount: amount = 1
+    selected = []
+    if len(foodlist) < amount:
+        amount = len(foodlist)
+    selected = random.sample(foodlist, amount)
+    output = str(selected)
+    output = output[1:len(output)-1]
+    output = output.replace("'", "")
+    output = output.replace(" ", "")
+    output = output.replace(",", "、")
+    j = random.randint(0, len(prefixs)-1)
+    await interaction.response.send_message(f"{prefixs[j]} **{output}** {ends[j]}")
+
+
+# --- Command: addfood ---
+@app_commands.command(name="addfood", description=f"[隨機] 新增食物到食物清單")
+@app_commands.describe(food="食物")
+async def addfood(interaction: discord.Interaction, food: str):
+    with open("foodlist.txt","r",encoding="UTF-8") as f:
+        contents = f.readlines()
+        foodlist = [i.strip() for i in contents]
+    if food not in foodlist:
+        with open("foodlist.txt","w",encoding="UTF-8") as f:
+            foodlist.append(food)
+            for i in foodlist: f.write(f"{i}\n")
+        await interaction.response.send_message(f"成功新增{food}")
+    else:
+        await interaction.response.send_message(f"{food}已存在")
---
+++ main.py
@@ -0,0 +1,127 @@
+# ----------------------------------------------- #
+#                  2024.9.21                      #
+#         VeryUsefulBot by Dfficult               #
+# https://www.github.com/dfficult/very_useful_bot #
+# ----------------------------------------------- #
+
+# Instructions: 1. Enter your token in line 27
+
+# === main.py ===
+import discord, datetime, asyncio
+from discord.ext import commands, tasks
+try:
+    from eat import *
+    from vub_math import *
+    from money import *
+    from dates import *
+    from codes import *
+    from notice import *
+except Exception as e:
+    print(e)
+    print("Try running 'main.py' again in the '/dc_bot' directory")
+    input("Press Enter to exit ...")
+    exit()
+
+
+# --- Discord bot token ---
+TOKEN = "TOKEN"
+if TOKEN == "TOKEN":
+    x = input("Token is not specified, enter your token or leave it blank to exit: ")
+    if x == '': exit()
+    else: TOKEN = x
+
+
+# --- Create bot instance ---
+bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
+
+
+# --- Bot loop ---
+@tasks.loop(minutes=1)
+async def notice_loop():
+    result = check_notice()
+    if result:
+        for i in result:
+            # result = [..., [user, channel, embed], ...]
+            channel = bot.get_channel(i[1])
+            await channel.send(f"<@{i[0]}>",embed=i[2])
+
+
+# --- Sync bot loop ---
+async def sync_task_to_minute():
+    now = datetime.datetime.now()
+    # Calculate the time to wait until the next minute starts
+    seconds_to_wait = 60 - now.second
+    await asyncio.sleep(seconds_to_wait)
+    notice_loop.start()
+
+
+# --- Load bot ---
+@bot.event
+async def on_ready():
+    slash = await bot.tree.sync()
+    print(f"{bot.user} 已成功登入，並已載入{len(slash)}個指令")
+    # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="YouTube"))
+    await bot.change_presence(activity=discord.Game(name="Grand Theft Auto VI"))
+    # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Never Gonna Give You Up"))
+    await sync_task_to_minute()
+
+
+
+# --- Actions when bot receives any message ---
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content == "hello world":
+        await message.channel.send("Hello World!")
+    
+
+# --- An example command ---
+@app_commands.command(name="vubhelp", description="所有指令說明")
+async def vubhelp(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title="歡迎使用 VeryUsefulBot",
+        description="前往指令列表 ➡️ https://www.github.com/dfficult/very_useful_bot"
+    )
+    await interaction.response.send_message(embed=embed)
+
+
+# --- Add all commands to the bot ---
+bot.tree.add_command(vubhelp)
+# notice.py
+bot.tree.add_command(notice)
+bot.tree.add_command(delnotice)
+# code.py
+bot.tree.add_command(code)
+bot.tree.add_command(submit_code)
+# dates.py
+bot.tree.add_command(today)
+bot.tree.add_command(daysleft)
+# eat.py
+bot.tree.add_command(eat)
+bot.tree.add_command(addfood)
+# vub_math.py
+bot.tree.add_command(correlation)
+bot.tree.add_command(simfrac)
+bot.tree.add_command(factorize)
+bot.tree.add_command(solve21)
+bot.tree.add_command(solve31)
+bot.tree.add_command(rand)
+bot.tree.add_command(dice)
+bot.tree.add_command(vector)
+bot.tree.add_command(vectorl)
+bot.tree.add_command(surface)
+bot.tree.add_command(average)
+bot.tree.add_command(det3)
+bot.tree.add_command(det2)
+bot.tree.add_command(invrmtx2)
+bot.tree.add_command(p)
+bot.tree.add_command(c)
+# money.py
+bot.tree.add_command(mlend)
+bot.tree.add_command(mhistory)
+bot.tree.add_command(mdelete)
+
+
+# Run the bot
+bot.run(TOKEN)
